3df0097c97c0ea91462cab02ac1befb63f0f6448
Mohamed Ayman <2moo.ayman@gmail.com>
2026-01-01 21:59:51 +0200

    Update pom.xml and module-info for database support

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.mycompany.java.server.project {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
+    requires java.sql;
 
     opens com.mycompany.java.server.project to javafx.fxml;
     exports com.mycompany.java.server.project;
